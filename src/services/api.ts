6af2768aa18c4202ce63edfb1d4afb1447381008
Maykel Santos <maykelsantoz@gmail.com>
2021-03-21 01:34:14 -0300

    Adicional de variaveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
 
-const apiURL =
-  process.env.NODE_ENV === "production"
-    ? "https://dtmoney-maykelsantoz.vercel.app/api"
-    : "http://localhost:3000/api";
+const apiURL = "https://localhost/api";
 
 export const api = axios.create({
   baseURL: apiURL,
