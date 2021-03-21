7d961fa88b89d9eaadeb36cd13cb11ca8ae8e91c
Maykel Santos <maykelsantoz@gmail.com>
2021-03-21 01:38:17 -0300

    Adicional de variaveis de ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const apiURL = "https://localhost/api";
+//const apiURL = "https://localhost/api";
 
 export const api = axios.create({
-  baseURL: apiURL,
+  baseURL: process.env.NODE_ENV,
 })
