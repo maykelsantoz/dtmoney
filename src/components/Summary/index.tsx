d3cd06b8c9949a6f26e05caf6fc67089f9c0c857
Maykel Santos <maykelsantoz@gmail.com>
2021-03-20 22:22:52 -0300

    Adicionando as primeiras partes do projeto

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import incomeIng from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
+
+import { Container } from "./styles";
+
+export function Summary() {
+  //const { transactions } = useContext(TransactionsContext);
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeIng} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  )
+}
